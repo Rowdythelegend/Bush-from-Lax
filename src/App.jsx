9fd4cf2dc8970249e5dc45a0a4b9a75728b4d8d5
Ndumiso (The Clairvoyant) <nslegend8@gmail.com>
2026-05-03 22:24:24 +0200

    fix: Remove TypeScript-isms from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ import {
   AlertCircle
 } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
-import { clsx, type ClassValue } from 'clsx'
+import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-function cn(...inputs: ClassValue[]) {
+function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
 
